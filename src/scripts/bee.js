e0768af01ef7bf8f1d71a0996949686ebfdbb511
jerryngton <jerryngtond@gmail.com>
2018-09-07 12:58:58 +0300

    hurry

--- src/scripts/bee.js
+++ src/scripts/bee.js
@@ -1,96 +1,3 @@
-/*
-function getStyle(el, styleProp) {
-    var value, defaultView = (el.ownerDocument || document).defaultView;
-    // W3C standard way:
-    if (defaultView && defaultView.getComputedStyle) {
-        // sanitize property name to css notation
-        // (hypen separated words eg. font-Size)
-        styleProp = styleProp.replace(/([A-Z])/g, "-$1").toLowerCase();
-        return defaultView.getComputedStyle(el, null).getPropertyValue(styleProp);
-    } else if (el.currentStyle) { // IE
-        // sanitize property name to camelCase
-        styleProp = styleProp.replace(/\-(\w)/g, function(str, letter) {
-            return letter.toUpperCase();
-        });
-        value = el.currentStyle[styleProp];
-        // convert other units to pixels on IE
-        if (/^\d+(em|pt|%|ex)?$/i.test(value)) {
-            return (function(value) {
-                var oldLeft = el.style.left, oldRsLeft = el.runtimeStyle.left;
-                el.runtimeStyle.left = el.currentStyle.left;
-                el.style.left = value || 0;
-                value = el.style.pixelLeft + "px";
-                el.style.left = oldLeft;
-                el.runtimeStyle.left = oldRsLeft;
-                return value;
-            })(value);
-        }
-        return value;
-    }
-}
-
-var moveBlock = document.querySelector('.move-wrapper');
-var moveBlockHeightValue = getStyle(moveBlock, 'height').replace('px','');
-var windowHeightValue = document.body.scrollHeight.toString().replace('px', '');
-
-document.addEventListener('wheel', function(e) {
-    var html = document.documentElement;
-    var body = document.body;
-    var beeTopStyle = getStyle(moveBlock, 'top');
-    var beeTopStyleValue = beeTopStyle.replace('px','');
-
-    if (beeTopStyleValue > windowHeightValue - moveBlockHeightValue ) {
-        moveBlock.style.top = html.clientTop + 'px';
-    }   else {
-        var scrollTop = html.scrollTop || body && body.scrollTop || 0;
-        scrollTop -= (html.clientTop - 68);
-        //scrollTop -= (html.clientTop - Math.floor(moveBlockHeightValue / 2));
-        computedStyle = scrollTop + 'px';
-        moveBlock.style.top = computedStyle;
-    }
-}, true);
-
-*/
-
-/*
-function getStyle(el, styleProp) {
-    var value, defaultView = (el.ownerDocument || document).defaultView;
-    // W3C standard way:
-    if (defaultView && defaultView.getComputedStyle) {
-        // sanitize property name to css notation
-        // (hypen separated words eg. font-Size)
-        styleProp = styleProp.replace(/([A-Z])/g, "-$1").toLowerCase();
-        return defaultView.getComputedStyle(el, null).getPropertyValue(styleProp);
-    } else if (el.currentStyle) { // IE
-        // sanitize property name to camelCase
-        styleProp = styleProp.replace(/\-(\w)/g, function(str, letter) {
-            return letter.toUpperCase();
-        });
-        value = el.currentStyle[styleProp];
-        // convert other units to pixels on IE
-        if (/^\d+(em|pt|%|ex)?$/i.test(value)) {
-            return (function(value) {
-                var oldLeft = el.style.left, oldRsLeft = el.runtimeStyle.left;
-                el.runtimeStyle.left = el.currentStyle.left;
-                el.style.left = value || 0;
-                value = el.style.pixelLeft + "px";
-                el.style.left = oldLeft;
-                el.runtimeStyle.left = oldRsLeft;
-                return value;
-            })(value);
-        }
-        return value;
-    }
-}
-
-var moveBlock = document.querySelector('.move-wrapper');
-var moveBlockHeightValue = getStyle(moveBlock, 'height').replace('px','');
-
-$(window).scroll(function() {
-    $('.move-wrapper').css('top', $(this).scrollTop() + "px");
-});
-*/
-
 var topField;
 $('.move-wrapper').hover(function() {
     $( this ).css({zIndex: 13});
@@ -125,8 +32,6 @@ $(document).ready(function(){
 
 
 $('.move-wrapper').sticky({zIndex: 13});
-$('.move-wrapper').css({'top': topField});
-
 
 $( window ).resize(function() {
     if ($( window ).width() < 400) {
@@ -137,22 +42,17 @@ $( window ).resize(function() {
 
 
 
-    }
-
-    else if ($( window ).width() >= 400 && $( window ).width() < 801)  {
+    } else if ($( window ).width() >= 400 && $( window ).width() < 801)  {
         topField = 340;
         $('.move-wrapper').unstick();
         $('.move-wrapper').css({'top': topField});
         $('.move-wrapper').sticky({topSpacing: topField, zIndex: 13});
 
-    }
-
-    else if ($( window ).width() >= 801 ) {
+    } else if ($( window ).width() >= 801 ) {
         topField = 170;
         $('.move-wrapper').unstick();
         $('.move-wrapper').css({'top': topField});
         $('.move-wrapper').sticky({topSpacing: topField, zIndex: 13});
-
     }
 });
 
--- src/scripts/fruto-map-popup.js
+++ src/scripts/fruto-map-popup.js
@@ -6,33 +6,6 @@ var pageHeaderToggle = document.querySelector('.page-header__toggle');
 var mainNav = document.querySelector('.main-nav');
 var moveBlock = document.querySelector('.move-wrapper');
 
-function getPosition(el) {
-    var xPos = 0;
-    var yPos = 0;
-
-    while (el) {
-        if (el.tagName == "BODY") {
-            // deal with browser quirks with body/window/document and page scroll
-            var xScroll = el.scrollLeft || document.documentElement.scrollLeft;
-            var yScroll = el.scrollTop || document.documentElement.scrollTop;
-
-            xPos += (el.offsetLeft - xScroll + el.clientLeft);
-            yPos += (el.offsetTop - yScroll + el.clientTop);
-        } else {
-            // for all other non-BODY elements
-            xPos += (el.offsetLeft - el.scrollLeft + el.clientLeft);
-            yPos += (el.offsetTop - el.scrollTop + el.clientTop);
-        }
-
-        el = el.offsetParent;
-    }
-    return {
-        x: xPos,
-        y: yPos
-    };
-}
-
-
 const currentLevelIdx = 1;
 function openLevelPopup(level) {
     level.addEventListener('click', function (evt) {
