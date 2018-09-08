b7095edac492ed6f1006a7a1d600e94b79768a4c
Gaponchuk Yuriy <gaponchykyn@gmail.com>
2018-09-08 16:12:44 +0500

    fix style

--- src/scripts/menu-navigation.js
+++ src/scripts/menu-navigation.js
@@ -3,4 +3,5 @@ var burgerIcon = document.querySelector('.page-header__toggle');
 burgerIcon.addEventListener('click', function (evt) {
     var menuBar = document.querySelector('.main-nav');
     menuBar.classList.toggle('main-nav--closed');
+    burgerIcon.classList.toggle('close');
 });
